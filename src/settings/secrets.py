8875f2e36fb577c4d7d063642684a4296f27ac5d
Ilya Siganov <ilya.blan4@gmail.com>
2017-12-07 19:08:28 +0600

    rm secrets

--- src/settings/secrets.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-token = '0772265e0772265e0772265ec1072dcf20007720772265e5d5470d1113126390b3cc41e'
-client_secret = 'PXN9E66j1A0r6AukDyO1'
-app_id = 6285694
-
-
-# group_id = -70232735  # wtfcute
-# group_id = -29937425  # seifuku_blog
